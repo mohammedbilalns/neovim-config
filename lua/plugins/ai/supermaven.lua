c4c8280a62fefdb99323b1f3b29208b5e455b1fe
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-03-25 15:54:21 +0530

    load  mason on bufreadpre , bufnewfile , load supermaven on insertenter

--- lua/plugins/ai/supermaven.lua
+++ lua/plugins/ai/supermaven.lua
@@ -1,5 +1,6 @@
 return {
 	"supermaven-inc/supermaven-nvim",
+  event = "InsertEnter",
 	config = function()
 		require("supermaven-nvim").setup({
       disable_inline_completion = true
--- lua/plugins/lsp-dap/mason-lspconfig.lua
+++ lua/plugins/lsp-dap/mason-lspconfig.lua
@@ -2,6 +2,7 @@
 -- Automatically enables LSP servers installed via Mason
 return {
   "williamboman/mason-lspconfig.nvim",
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "williamboman/mason.nvim",
     "neovim/nvim-lspconfig",
